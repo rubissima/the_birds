e1b68e65c62586d2b3845616937b15d1110a1097
Ruby Kane <rubykane@gmail.com>
2018-07-24 10:49:11 -0700

    changes from last night

--- birds.js
+++ birds.js
@@ -3,64 +3,35 @@
     var feedWidth = window.innerHeight - topHeight;
     document.getElementById("feed").style.height = feedWidth + "px";
 
-    function show(e) {
-        // Make a list
-        // var ul = document.createElement('ul');
-        // document.body.appendChild(ul);
 
-        // var seed = document.createElement('li');
-        // var li2 = document.createElement('li');
-        // ul.appendChild(li1);
-        // ul.appendChild(li2);
+    // function show(e) {
+    //     // Make a list
+    //     // var ul = document.createElement('ul');
+    //     // document.body.appendChild(ul);
 
-        // e.target refers to the clicked <li> element
-        // This is different than e.currentTarget which would refer to the parent <ul> in this context
-        var seed = e.currentTarget.id;
-        console.log(seed);
+    //     // var seed = document.createElement('li');
+    //     // var li2 = document.createElement('li');
+    //     // ul.appendChild(li1);
+    //     // ul.appendChild(li2);
 
-        div.addEventListener('click', show, false);
-    }
-    var img = new Image();
-    img.src = './flora/corn-seed.jpg';
-    img.onload = function() {
-        var pattern = dc.createPattern(img, 'no-repeat');
-        dc.fillStyle = pattern;
-        dc.fillRect(0, 0, 400, 400);
+    //     // e.target refers to the clicked <li> element
+    //     // This is different than e.currentTarget which would refer to the parent <ul> in this context
+    //     var seed = e.currentTarget.id;
+    //     console.log(seed);
+
+    //     div.addEventListener('click', show, false);
+    // }
+
+    var output = "<h1>SEEDS</h1>";
+    output += "<div>";
+    // Loop through the artists
+    for (var i in data.seeds) {
+        output += "<div> <h2>" + data.seeds[i].name.toUpperCase() + "</h2> <div><img class=\"round-img\" src=\"" + data.seeds[i].img + " /></div></div>";
     };
 
-    // var seeds = document.getElementsByClassName("seed");
-    // console.log(typeof(seeds));
-    // seeds.map(x => x.id);
-    // switch (seed) {
-    //     case nyjer:
-    //         // execute case x code block
-    //         break;
-    //     case blackSunflower:
-    //         // execute case x code block
-    //         break;
-    //     case stripedSunflower:
-    //         // execute case x code block
-    //         break;
-    //     case hulledSunflower:
-    //         // execute case x code block
-    //         break;
-    //     case millet:
-    //         // execute case x code block
-    //         break;
-    //     case milo:
-    //         // execute case x code block
-    //         break;
-    //     case peanuts:
-    //         // execute case x code block
-    //         break;
-    //     case safflower:
-    //         // execute case x code block
-    //         break;
-    //     case corn:
-    //         // execute case y code block
-    //         break;
-    // }
-    // console.log(seeds);
+    output += "</div>";
+    document.getElementById("seedSection").innerHTML = output;
+
 
     var canv = document.getElementById("canv");
     canv.width = feedWidth;
@@ -69,4 +40,20 @@
     var w = feedWidth - 200;
     var h = feedWidth - 200;
     refresh(dc, w, h, 0);
+    var img = new Image();
+    img.src = './flora/corn-seed.jpg';
+    var pattern = dc.createPattern(img, 'no-repeat');
+
+    function refresh(dc, width, height) {
+        dc.fillStyle = pattern;
+
+        dc.beginPath();
+        var cx = width / 2;
+        var cy = height / 2;
+        var rad = width * .45;
+
+        dc.arc(cx, cy, rad, 0, 2 * Math.PI, false);
+        dc.fill();
+
+    }
 })();
